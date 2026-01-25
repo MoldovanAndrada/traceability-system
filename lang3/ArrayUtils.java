e3413280119075233e22149ab676183df19b7011
MoldovanAndrada <50479889+MoldovanAndrada@users.noreply.github.com>
2026-01-25 20:39:46 +0200

    Fix reverseArray method signature in ArrayUtils

--- lang3/ArrayUtils.java
+++ lang3/ArrayUtils.java
@@ -1,4 +1,4 @@
 package org.apache.commons.lang3;
 public class ArrayUtils {
-    public static void reverseArray(char[] a) { if(a==null) return; int len = a.length; for(int i=0; i<len/2; i++) { char t = a[i]; a[i] = a[len-1-i]; a[len-1-i] = t; } }
+    public static void reverseArray() { if(a==null) return; int len = a.length; for(int i=0; i<len/2; i++) { char t = a[i]; a[i] = a[len-1-i]; a[len-1-i] = t; } }
 }
