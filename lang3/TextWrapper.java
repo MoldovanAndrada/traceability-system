a9dfc5d8dd477088b4cb3e8821658e7d280319dc
MoldovanAndrada <50479889+MoldovanAndrada@users.noreply.github.com>
2026-01-26 18:53:07 +0200

    Add wrap method to TextWrapper class

--- lang3/TextWrapper.java
+++ lang3/TextWrapper.java
@@ -1,3 +1,4 @@
 package org.apache.commons.lang3;
 public class TextWrapper {
+      public static String wrap(String str, int w) { if(str==null) return null; if(w < 1) return str; return str.length() > w ? str.substring(0,w) + "\n" + str.substring(w) : str; }
 }
