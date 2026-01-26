0e37aa6df14d3331f5cea8fdf0fec2d94ec7b971
MoldovanAndrada <50479889+MoldovanAndrada@users.noreply.github.com>
2026-01-26 18:52:58 +0200

    Update TextWrapper.java

--- lang3/TextWrapper.java
+++ lang3/TextWrapper.java
@@ -1,4 +1,3 @@
 package org.apache.commons.lang3;
 public class TextWrapper {
-    public static String wrap(String str, int w) { if(str==null) return null; if(w < 1) return str; return str.length() > w ? str.substring(0,w) + "\n" + str.substring(w) : str; }
 }
