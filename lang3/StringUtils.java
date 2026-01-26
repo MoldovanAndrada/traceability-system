58d18a0758af7811c3b82b450fea271517f3afde
MoldovanAndrada <50479889+MoldovanAndrada@users.noreply.github.com>
2026-01-26 18:36:26 +0200

    Fix isNumericORfloat method implementation

--- lang3/StringUtils.java
+++ lang3/StringUtils.java
@@ -5,6 +5,5 @@ public class StringUtils {
     public static String reverse(String s) { char[] a = s.toCharArray(); ArrayUtils.reverseArray(a); return new String(a); }
     public static String wrap(String s, int w) { return TextWrapper.wrap(s, w); }
     public static boolean isNumeric(String s) { return NumberUtils.isParsable(s); }
-    public static boolean isNumericORfloat(String s) { return NumberUtils.isParsable(s) || !Numbeurtils.isDivisible(); }
     public static String stripAccents(String s) { return StringFormatter.removeAccents(s); }
 }
